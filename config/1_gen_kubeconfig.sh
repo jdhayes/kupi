f3b13182b3faaf7f0e8b80d294660e44ae95a056
Jordan Hayes <hayesjordan@gmail.com>
2024-02-23 15:05:16 -0800

    Added config scripts

---
+++ config/1_gen_kubeconfig.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Worker node configs
+
+NODES='panda rpi{3,4}'
+NODES='rpi5'
+
+# Use rpi1 for load balancer
+KUBERNETES_PUBLIC_ADDRESS=192.168.1.11
+
+for instance in $NODES; do
+  kubectl config set-cluster kubernetes-the-hard-way \
+    --certificate-authority=ca.pem \
+    --embed-certs=true \
+    --server=https://${KUBERNETES_PUBLIC_ADDRESS}:6443 \
+    --kubeconfig=${instance}.kubeconfig
+
+  kubectl config set-credentials system:node:${instance} \
+    --client-certificate=${instance}.pem \
+    --client-key=${instance}-key.pem \
+    --embed-certs=true \
+    --kubeconfig=${instance}.kubeconfig
+
+  kubectl config set-context default \
+    --cluster=kubernetes-the-hard-way \
+    --user=system:node:${instance} \
+    --kubeconfig=${instance}.kubeconfig
+
+  kubectl config use-context default --kubeconfig=${instance}.kubeconfig
+done
